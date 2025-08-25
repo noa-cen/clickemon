43964ca646a11ade1eedc5a5a78b2612e3d24944
noa-cen <noacengarle@gmail.com>
2025-08-25 15:42:29 +0200

    home jsx and css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Home.css';
 
-export default function Home({ onFinish }) {
+export default function Home() {
   return (
     <section className="home">
         <img src='/assets/icons/logo.png' alt='logo' className='logo' />
         <h1>Clickémon</h1>
-        <button className="start-btn" onClick={onFinish}>
+        <button className="start-btn">
             Start
         </button>
         <article className='home-pokemon'>
-            <img src='/assets/images/pokemon/Charmander - 4.png' alt='Charmander' className='pokemon' />
-            <img src='/assets/images/pokemon/Bulbasaur - 1.png' alt='Bulbasaur' className='pokemon' />
-            <img src='/assets/images/pokemon/Squirtle - 7.png' alt='Squirtle' className='pokemon' />
+            <img src='/assets/images/pokemon/Charmander - 4.png' alt='Charmander' className='pokemon-starter' />
+            <img src='/assets/images/pokemon/Bulbasaur - 1.png' alt='Bulbasaur' className='pokemon-starter' />
+            <img src='/assets/images/pokemon/Squirtle - 7.png' alt='Squirtle' className='pokemon-starter' />
         </article>
     </section>
   );
